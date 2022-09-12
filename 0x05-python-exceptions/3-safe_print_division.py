bcc276fc502900fae125ce7cc93ce72dceeb7480
kirubelAtnafu <kiraatnafu@gmail.com>
2022-09-12 12:04:37 -0700

    three

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def safe_print_division(a, b)
+def safe_print_division(a, b):
+
     try:
         response = a / b
     except ZeroDivisionError:
@@ -7,5 +8,4 @@ def safe_print_division(a, b)
     finally:
         print('Inside result: {}'.format(response))
 
-return response
-
+        return response
